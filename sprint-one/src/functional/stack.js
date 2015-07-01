33c135d5f382cd97899ce233c6b5677641a90bd2
Kiri Strack-Grose <kiristrackgrose@gmail.com>
2015-07-01 12:06:39 -0700

    functional stack tests passed

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -4,14 +4,25 @@ var Stack = function(){
   // Use an object with numeric keys to store values
   var storage = {};
 
+  //length property:
+  var length = 0;
+
   // Implement the methods below
   someInstance.push = function(value){
+    storage[length++] = value;
   };
 
   someInstance.pop = function(){
+    if (length !== 0) {
+      return storage[--length];
+    } else {
+      return storage[length-1];
+    }
+
   };
 
   someInstance.size = function(){
+    return length;
   };
 
   return someInstance;
