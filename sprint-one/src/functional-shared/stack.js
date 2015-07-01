8f6c86333e3f640d58f3ca4ee78cf87bf4ea3169
Kiri Strack-Grose <kiristrackgrose@gmail.com>
2015-07-01 12:30:31 -0700

    beginnings of functional-shared

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,6 +1,16 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+
+  var anInstance = {
+    length: 0;
+  };
+
+  var storage = {};
+
+  var push = function(value) {
+    this[length++] = value;
+  }
 };
 
 var stackMethods = {};
