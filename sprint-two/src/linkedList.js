4b7feaac5c1dc3f76ea64c7ec5751f05b7afcc2e
kstrack-grose <kiristrackgrose@gmail.com>
2015-07-02 16:40:42 -0700

    started on linked lists

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,34 @@ var LinkedList = function(){
   list.tail = null;
 
   list.addToTail = function(value){
+    var node = Node(value);
+    if(list.head === null) {
+      list.head = node;
+    }
+
+    if(list.tail !== null) {
+      list.tail.next = node;
+    }
+    list.tail = node;
+
+
   };
 
-  list.removeHead = function(){
+  list.removeHead = function(){  
+    if(list.head !== null){
+      var result = list.head.value;
+      list.head = list.head.next;
+      return result;
+    }
   };
 
   list.contains = function(target){
+    var curNode = list.head;
+    while(curNode !== null) {
+      if(curNode.value === target) {
+        return curNode;
+      }
+    } 
   };
 
   return list;
