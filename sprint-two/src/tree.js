722301cc93eaba78f57a75470fbcbe1afbf24c22
kstrack-grose <kiristrackgrose@gmail.com>
2015-07-03 12:05:27 -0700

    tree is done and successful

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,9 +1,9 @@
 var Tree = function(value){
   var newTree = {};
   newTree.value = value;
-
-  // your code here
-  newTree.children = null;  // fix me
+  newTree.addChild = treeMethods.addChild;
+  newTree.contains = treeMethods.contains;
+  newTree.children = [];
 
   return newTree;
 };
@@ -14,12 +14,30 @@ var Tree = function(value){
 
 var treeMethods = {};
 
-treeMethods.addChild = function(value){
 
+treeMethods.addChild = function(value){
+	this.children[this.children.length] = Tree(value);
 };
 
 treeMethods.contains = function(target){
+	/* recursive function here */
+	var result = false;
+	var searchChildren = function(tree, goal) {
+		if (tree.value === goal) {
+			result = true;
+			return;
+		} else {
+			for (var i = 0; i < tree.children.length; i++) {
+				searchChildren(tree.children[i], goal);
+			}
+		}
+	}
 
+	searchChildren(this, target);
+
+	return result;
+
+//	return !!(searchChildren(this, target));
 };
 
 
